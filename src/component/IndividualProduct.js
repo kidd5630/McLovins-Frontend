ea6a4ea4e7254f807766a8a6ba382c4549b67974
peter <sunghnkim95@gmail.com>
2021-10-29 16:11:16 -0400

    working on carts

--- src/component/IndividualProduct.js
+++ src/component/IndividualProduct.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom'
 import { BASE_URL} from '../api'
 import EditProduct from './EditProduct';
-
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 
 const IndividualProduct = ({userToken, isAdmin, allProducts, setAllProducts, selectedProduct, productName, productDescript, setProductName, setProductDescript, productPrice, setProductPrice, productCategory, setProductCategory, productQuantity, setProductQuantity, productPhoto, setProductPhoto}) => {
     const [isActiveEdit, setActiveEdit] = useState("false");
+    const [valueQuant, setValueQuant] = useState(0)
     const ToggleClass = () => {
         setActiveEdit(!isActiveEdit);
     };
@@ -14,6 +16,7 @@ const IndividualProduct = ({userToken, isAdmin, allProducts, setAllProducts, sel
     const filteredProduct = allProducts.filter(product => {
         return parseInt(productid) == product.id
     })[0]
+    
     return (
         <> 
             <div className="ip">
@@ -25,7 +28,18 @@ const IndividualProduct = ({userToken, isAdmin, allProducts, setAllProducts, sel
                             <p className="innerText"> {filteredProduct.description}</p>
                             <p className="innerText">Price: ${filteredProduct.price}</p>
                             <p className="innerText">Available Quantity{filteredProduct.quantity}</p>
-                            
+                            <form className="addRemoveProduct" onSubmit={()=>{
+
+                            }}>
+                            <button className="removeProduct" onClick={()=>{
+                                setValueQuant(valueQuant - 1)
+                            }}><RemoveIcon></RemoveIcon></button>
+                            <input type="number" value={valueQuant}></input>
+                            <button className="addProduct" onClick={()=>{
+                                setValueQuant(valueQuant + 1)
+                            }}><AddIcon></AddIcon></button>
+                            <button type="submit">Add To Cart</button>
+                            </form>
                         </div> 
                         {isAdmin
                         ?
